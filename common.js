5bbcdabdf3148085f4ebafe9f3c7fbe98a525567
Jayden Downes <35357527+JaydenDownes@users.noreply.github.com>
2023-07-30 22:28:29 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,232 @@
+var moment = require('moment');
+var timez = require('moment-timezone');
+var _ = require('underscore');
+var suncalc = require('suncalc');
+var request = require('request');
+
+var timers = {};
+
+
+////////////////////////////
+// MESSAGE/RULES PROCESSING
+////////////////////////////
+
+exports.processMessage = function(packet){
+  // process rules that are contingent on msg events and update the DB model
+
+  // just ignore packets of devices that are not in our DB
+  if(DB.devices[packet.group]){
+    var oldLevel = DB.devices[packet.group].level;
+
+    updateDeviceData(packet.group,packet.level);
+
+    // info packets come from a bootstrap operation, we dont want to process rules for them
+    if(packet.type!='info'){
+      processRules(packet.group,DB.devices[packet.group]);
+    }
+
+    // TODO: not sure how to accomplish this without knowing that the status event was driven by a button press  (is sourceunit == 1 what I am after?)
+    // kill any timers that might be running for this group if we got a manual state change
+    //if (packet.level > 0 && packet.sourceunit > 1) {
+    //    killTimer(packet.group);
+    //}
+
+    // only notify the UI of stuff that is not hidden and it is a new level
+    if(DB.devices[packet.group].visible && oldLevel!=packet.level){
+      var sendMsg = {type:"update_status",group:packet.group,level:parseInt(packet.level),name:DB.devices[packet.group].name,location:DB.devices[packet.group].location};
+      console.log('sending');
+      console.log(sendMsg);
+      var realtimepacket = sendMsg;
+      IO.emit('statusStream', realtimepacket);
+    }
+  }
+}
+
+exports.doCommands = function(cmdArray,level) {
+  console.log(cmdArray);
+  processCommands(cmdArray,level);
+}
+
+function updateDeviceData(group, level){
+  // record the timestamp of this level change and update the current level
+  console.log('updating data for group '+group);
+  DB.devices[group].lastchange = moment().unix();
+  DB.devices[group].level = level;
+}
+
+function processCommands(cmdArray,level) {
+    // how many commands are we looping over?
+    var i = 0;
+
+    // define the delayed loop function
+    function delayedLoop() {
+
+        var waitTillNextCommand = 0;
+        var command = cmdArray[i];
+
+        if(command.type=='lighting') {
+            processLightingCommand(command,level);
+        }
+        else if(command.type == 'delay') {
+            waitTillNextCommand = command.delay;
+        }
+        else if(command.type == 'url') {
+            processUrlCommand(command);
+        }
+        else if(command.type == 'email') {
+            // TODO: email someone
+        }
+        else if(command.type == 'sms') {
+            // TODO: send sms
+        }
+        else if(command.type == 'rule') {
+            // TODO: enable/disable a rule
+        }
+        else if(command.type == 'task') {
+            // TODO: enable/disable a task
+        }
+        else if(command.type == 'scene') {
+            // TODO: trigger a scene
+        }
+        else {
+            console.log('Did not recognize command:');
+            console.log(command);
+        }
+
+        // if the end of the array has been reached, stop
+        if(++i == cmdArray.length) {
+            return;
+        }
+
+        // recursively call the delayed loop function with a delay
+       setTimeout(delayedLoop, 1000 * waitTillNextCommand);
+    }
+    // kick off these commands one at a time
+    delayedLoop();
+}
+
+function processRules(id,device) {
+    // first off we need to extract some values so we can process the expression
+    var dd = timez().tz(CONFIG.location.timezone);
+
+    var times = suncalc.getTimes(dd,CONFIG.location.latitude,CONFIG.location.longitude);
+
+    // variables we can use in our expressions
+    var time = timez().tz(CONFIG.location.timezone).format('HH:mm'); // the current time
+    var sunset = timez(times.sunset).tz(CONFIG.location.timezone).format('HH:mm'); // sun starts to set
+    var dusk = timez(times.dusk).tz(CONFIG.location.timezone).format('HH:mm'); // sun has fully set and it is starting to get dark
+    var sunrise = timez(times.sunrise).tz(CONFIG.location.timezone).format('HH:mm'); // sun has started to rise
+    var dawn = timez(times.dawn).tz(CONFIG.location.timezone).format('HH:mm'); // sun has not risen but it is starting to get light
+    var group = id; // group address of the group that triggered the rules engine
+    var level = device.level; // this is the level of the group that triggered the rules engine, the rules say this is 0-100
+
+    console.log('---------------START PROCESSING RULES---------------');
+    console.log('rule vars: ' + time + ' ' + sunset + ' ' + dusk + ' ' + dawn + ' ' + sunrise + ' ' + group + ' ' + level);
+
+    _.each(DB.rules,function(rule,ind){
+        // only process enabled rules
+        if(rule.enabled){
+            if(eval(rule.expression)) {
+                console.log('----------- BINGO --------------')
+                console.log(rule);
+                var therules = rule.commands;
+                processCommands(therules,level);
+            }
+        }
+        if(DB.rules.length==ind+1){
+          console.log('---------------DONE PROCESSING RULES---------------');
+        }
+    })
+}
+
+function processUrlCommand(command) {
+    request(command.url, function (error, response, body) {
+        if (error) {
+          console.log('got back an error when calling '+command.url+': '+error.message);
+        } else if (response.statusCode != 200) {
+          console.log('got back an error when calling '+command.url+': '+response.statusCode);
+        }
+    });
+}
+
+function processLightingCommand(command,level) {
+    // only need to adjust the actual lighting level (which we will cast to 0-100) if it is not at the level the scene says it needs to be
+    var changeto = parseInt(command.level);
+    var cmdtext = 'ramp';
+
+    if(changeto!=DB.devices[command.group].level){
+      // they might have a command passed from a rule, and want to use the level from the group that triggered the rule (useful for syncing)
+      if(command.level == 'level') {
+        changeto = level;
+      }
+
+      if(changeto==100){
+        cmdtext = 'on';
+      }
+      else if (changeto==0) {
+        cmdtext = 'off';
+      }
+
+      // if they want to fade up slowly
+      if(command.delay) {
+        cmdtext = 'ramp';
+      }
+
+      sendLightingCommand(command.group,cmdtext,changeto,command.delay,DB.devices[command.group].vendor);
+    }
+
+    // TIMERS SUPPORT
+    if(command.timeout > 0 && command.level > 0){
+      // if there is already a timer running, lets clear it and start it up again
+      killTimer(command.group);
+
+      console.log('TIMER: will turn off group '+command.group+' in '+command.timeout+' seconds')
+      timers[command.group] = setTimeout(function(){
+        sendLightingCommand(command.group,'off',0,0,DB.devices[command.group].vendor);
+      }, command.timeout*1000);
+    }
+
+    function sendLightingCommand(device,command,level,delay,vendor){
+      if(vendor=='cbus'){
+        var cmd = CBUS.cmdString(device,command,level,delay);
+        console.log('DEBUG!!!!');
+        console.log(cmd);
+        CBUS.write(cmd);
+      }
+      // TODO: integration with other vendors here?
+    }
+}
+
+function killTimer(group) {
+    if(timers[group]) {
+        console.log('Killing TIMER: '+group);
+        clearTimeout(timers[group]);
+    }
+}
+
+
+////////////////////////////
+// OTHER METHODS
+////////////////////////////
+
+// we store devices funky natively for speed reasons, this converts to an array that devs will like better
+exports.deviceObjToArray = function(devices){
+    //console.log(devices);
+    var groups = _.keys(devices);
+    var returnval = [];
+    _.each(groups,function(group){
+        var obj = {group:group};
+        obj.level = parseInt(devices[group].level);
+        obj.name = devices[group].name;
+        obj.location = devices[group].location;
+        obj.lastchange = devices[group].lastchange;
+        obj.type = devices[group].type;
+        obj.vendor = devices[group].vendor;
+
+        // only pop it on the list if it is visible
+        if(devices[group].visible){
+            returnval.push(obj);
+        }
+    });
+    return returnval;
+}
--- server.js
+++ server.js
@@ -1,22 +1,152 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-app.use(express.json());
+console.log("");
+console.log(" ██████ ██████  ██    ██ ███████     ███████ ███    ███  █████  ██████  ████████     ██   ██  ██████  ███    ███ ███████");
+console.log("██      ██   ██ ██    ██ ██          ██      ████  ████ ██   ██ ██   ██    ██        ██   ██ ██    ██ ████  ████ ██      ");
+console.log("██      ██████  ██    ██ ███████     ███████ ██ ████ ██ ███████ ██████     ██        ███████ ██    ██ ██ ████ ██ █████   ");
+console.log("██      ██   ██ ██    ██      ██          ██ ██  ██  ██ ██   ██ ██   ██    ██        ██   ██ ██    ██ ██  ██  ██ ██      ");
+console.log(" ██████ ██████   ██████  ███████     ███████ ██      ██ ██   ██ ██   ██    ██        ██   ██  ██████  ██      ██ ███████ ");
+console.log("");
+console.log("██ ███    ██ ████████ ███████ ██████  ███████  █████   ██████ ███████");
+console.log("██ ████   ██    ██    ██      ██   ██ ██      ██   ██ ██      ██     ");
+console.log("██ ██ ██  ██    ██    █████   ██████  █████   ███████ ██      █████  ");
+console.log("██ ██  ██ ██    ██    ██      ██   ██ ██      ██   ██ ██      ██     ");
+console.log("██ ██   ████    ██    ███████ ██   ██ ██      ██   ██  ██████ ███████");
+console.log("");
+console.log("Version 2.0, Created by Jayden Downes");
+console.log("");
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// Import required modules
+var _ = require('underscore');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+var socketIo = require('socket.io');
+var cron = require('cron').CronJob;
+
+// Load configuration and common files
+CONFIG = require('./config');
+COMMON = require('./common');
+DB = require('./db.json');
+
+////////////////////////
+// LOAD THE CONNECTORS
+////////////////////////
+// If configuration enables C-Gate, initialize C-Gate connection
+if(CONFIG.cgate){
+  console.log('(\u001b[32m#\u001b[0m) Initializing the cgate connector');
+  CBUS = require('./cgate').init();
+}
+
+// Initialize Express.js for main app and set up HTTP server and Socket.IO
+var app = express();
+var server = http.Server(app);
+IO = socketIo(server);
+
+////////////////////////
+// WEBSERVER SETUP
+////////////////////////
+// Initialize Express.js for light interface and attach it to main app
+var lightApp = express();
+app.use('/light-interface', lightApp);
+
+// Set up static files directory
+app.use('/light-interface', express.static(path.join(__dirname, 'light-interface')));
+
+// Routes for light interface
+lightApp.get('/cgate', function (req, res) {
+    if (req.query.cmd) {
+      var command = req.query.cmd.trim() + '\r';
+      console.log('remoteCommand : ' + command);
+      CBUS.write(command);
+      res.json({ status: 'ok', executed: req.query.cmd});
+    }
+    else {
+      res.json({ status: 'error', message: 'you must specify a command!'},400);
+    }
+  });
+lightApp.get('/cmd', function (req, res) {
+    console.log(req.query);
+    var commandArray = [{type:'lighting',group:req.query.device,level:parseInt(req.query.level),delay:parseInt(req.query.delay),timeout:parseInt(req.query.timeout)}];
+    COMMON.doCommands(commandArray);
+    res.json(JSON.stringify({ status: 'ok'}));
+});
+lightApp.get('/locations', function (req, res) {
+    // loop over the keys and build the response (an array of objects)
+    var resp = _.uniq(_.pluck(COMMON.deviceObjToArray(DB.devices), 'location'));
+    resp = _.sortBy(resp, function(str){ return str; });
+    res.json(resp);
+});
+lightApp.get('/scenes', function (req, res) {
+    res.json(DB.scenes);
+});
+lightApp.get('/devices', function (req, res) {
+    res.json(COMMON.deviceObjToArray(DB.devices));
 });
 
-app.get('/api/tasks', (req, res) => {
+//////////////////////////
+// GLOBAL HELPER METHODS
+//////////////////////////
+var cronjobs = {};
+var cronjob = require('cron').CronJob;
+// load up everything currently in the DB
+_.each(DB.tasks,function(task){
+    // only create cron jobs for tasks that are enabled
+    if (task.enabled) {
+        addCronJob(task.id,task.cronstring,task.expression,task.commands,CONFIG.location.timezone);
+    }
+})
+
+function addCronJob(id,cronstring,expression,commands,timezone){
+    try {
+        console.log('(\u001b[32m#\u001b[0m) Adding cronjob task: '+id);
+        cronjobs[id] = new cronjob(cronstring, function(){
+            console.log('(\u001b[32m#\u001b[0m) Starting Cronjob Task: '+id);
+            // some of these tasks may have a conditional expression, some can just run
+            if(expression){
+                if(eval(expression)) {
+                    COMMON.doCommands(commands);
+                }
+            }
+            else {
+                COMMON.doCommands(commands);
+            }
+        }, function(){console.log('(\u001b[32m#\u001b[0m) Cronjob stopped: '+id)}, true, timezone);
+    } catch(ex) {
+        console.log("(\u001b[31m#\u001b[0m) cronstring pattern not valid for "+id+": "+cronstring);
+    }
+}
+
+function deleteCronJob(id) {
+    console.log('(\u001b[32m#\u001b[0m) Killing cronjob '+id);
+    if(cronjobs[id]){
+        cronjobs[id].stop();
+    }
+    delete cronjobs[id];
+}
+
+
+//////////////////////////
+// CONFIGURATION INTERFACE
+//////////////////////////
+// Initialize Express.js for configuration interface and attach it to main app
+var configApp = express();
+app.use('/configuration-interface', configApp);
+
+// Enable JSON body parsing for configuration interface
+configApp.use(express.json());
+
+// Routes for configuration interface
+configApp.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './configuration-interface/index.html'));
+});
+configApp.get('/api/configuration/tasks', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let tasks = JSON.parse(data).tasks;
         res.json({ tasks: tasks });
     });
 });
-
-app.put('/api/tasks/:id', (req, res) => {
+configApp.put('/api/configuration/tasks/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -33,8 +163,7 @@ app.put('/api/tasks/:id', (req, res) => {
         }
     });
 });
-
-app.post('/api/tasks', (req, res) => {
+configApp.post('/api/configuration/tasks', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -48,8 +177,7 @@ app.post('/api/tasks', (req, res) => {
         });
     });
 });
-
-app.delete('/api/tasks/:id', (req, res) => {
+configApp.delete('/api/configuration/tasks/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -68,15 +196,14 @@ app.delete('/api/tasks/:id', (req, res) => {
 });
 
 
-app.get('/api/rules', (req, res) => {
+configApp.get('/api/configuration/rules', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let rules = JSON.parse(data).rules;
         res.json({ rules: rules });
     });
 });
-
-app.put('/api/rules/:id', (req, res) => {
+configApp.put('/api/configuration/rules/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -93,8 +220,7 @@ app.put('/api/rules/:id', (req, res) => {
         }
     });
 });
-
-app.post('/api/rules', (req, res) => {
+configApp.post('/api/configuration/rules', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -108,8 +234,7 @@ app.post('/api/rules', (req, res) => {
         });
     });
 });
-
-app.delete('/api/rules/:id', (req, res) => {
+configApp.delete('/api/configuration/rules/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -127,15 +252,15 @@ app.delete('/api/rules/:id', (req, res) => {
     });
 });
 
-app.get('/api/scenes', (req, res) => {
+
+configApp.get('/api/configuration/scenes', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let scenes = JSON.parse(data).scenes;
         res.json({ scenes: scenes });
     });
 });
-
-app.put('/api/scenes/:id', (req, res) => {
+configApp.put('/api/configuration/scenes/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -152,8 +277,7 @@ app.put('/api/scenes/:id', (req, res) => {
         }
     });
 });
-
-app.post('/api/scenes', (req, res) => {
+configApp.post('/api/configuration/scenes', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -168,7 +292,7 @@ app.post('/api/scenes', (req, res) => {
     });
 });
 
-app.delete('/api/scenes/:id', (req, res) => {
+configApp.delete('/api/configuration/scenes/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -186,15 +310,15 @@ app.delete('/api/scenes/:id', (req, res) => {
     });
 });
 
-app.get('/api/devices', (req, res) => {
+
+configApp.get('/api/configuration/devices', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let devices = JSON.parse(data).devices;
         res.json({ devices: devices });
     });
 });
-
-app.put('/api/devices/:id', (req, res) => {
+configApp.put('/api/configuration/devices/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -210,8 +334,7 @@ app.put('/api/devices/:id', (req, res) => {
         }
     });
 });
-
-app.post('/api/devices', (req, res) => {
+configApp.post('/api/configuration/devices', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -225,8 +348,7 @@ app.post('/api/devices', (req, res) => {
         });
     });
 });
-
-app.delete('/api/devices/:id', (req, res) => {
+configApp.delete('/api/configuration/devices/:id', (req, res) => {
     fs.readFile('db.json', (err, data) => {
         if (err) throw err;
         let db = JSON.parse(data);
@@ -243,4 +365,12 @@ app.delete('/api/devices/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+//////////////////////////
+// SERVER INITIALIZATION
+//////////////////////////
+// Start server on configured host and port
+server.listen(CONFIG.webserver.port,CONFIG.webserver.host);
+
+// Log successful server start
+console.log('(\u001b[32m#\u001b[0m) Standalone Configuration Interface is running on: http://'+CONFIG.webserver.host+':'+CONFIG.webserver.port+'/configuration-interface');
+console.log('(\u001b[32m#\u001b[0m) Standalone Lighting Control Interface is running on: http://'+CONFIG.webserver.host+':'+CONFIG.webserver.port+'/light-interface');
